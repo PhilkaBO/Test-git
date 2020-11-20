d8cb098ff290049ef92a3fab5e1b5a7e16e5851c
PhilkaBO <ledentsov.vladislav.2014.b@mail.ru>
2020-11-20 19:58:53 +0700

    Create double my_pow

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+double my_pow(double, unsigned int);
+
+int main()
+{
+
+}
+
+double my_pow(double value, unsigned int pow) {
+    double result = 1;
+    while (pow != 0) {
+        result = result * value;
+        pow--;
+    }
+    return result;
+}
